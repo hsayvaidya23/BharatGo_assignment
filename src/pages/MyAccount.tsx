7943c8678a13fc20e869bf1006e58ec38f62bf8f
Yash Vaidya <bobbynaldocr7@gmail.com>
2025-02-13 17:30:47 +0530

    my account customization

--- src/pages/MyAccount.tsx
+++ src/pages/MyAccount.tsx
@@ -1,24 +1,35 @@
-import Layout from './../components/Layout';
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Layout from '../components/Layout';
+import { ShoppingCartContext } from '../contexts';
 
 function MyAccount() {
+  const context = useContext(ShoppingCartContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!context.user) {
+      navigate('/'); // Redirect to login page if user is not logged in
+    }
+  }, [context.user, navigate]);
+
+  if (!context.user) {
+    return <p>Loading...</p>; // Display a loading message while checking authentication
+  }
+
   return (
     <Layout>
       <p className="m-4">My Account</p>
       <div className="flex flex-col items-center justify-center w-2/4 h-3/4 border border-black rounded-lg p-8">
         <figure className="flex items-center justify-center flex-col">
-          <p className="mb-5 mt-5 font-medium">Created by:</p>
+          <p className="mb-5 mt-5 font-medium">Welcome, {context.user.displayName}!</p>
           <img
-            src="https://avatars.githubusercontent.com/u/90741749?v=4"
-            alt="EdCenten0"
+            src={context.user.photoURL || "/default-avatar.png"}
+            alt={context.user.displayName || "User"}
             className="border border-slate-950 rounded-full"
           />
-          <p className="font-bold mt-2 mb-2">Yash Vaidya</p>
-          <a
-            href="https://github.com/hsayvaidya23"
-            className="font-light underline"
-          >
-            @yashvaidya23👈
-          </a>
+          <p className="font-bold mt-2 mb-2">{context.user.displayName}</p>
+          <p className="font-light">{context.user.email}</p>
         </figure>
       </div>
     </Layout>
